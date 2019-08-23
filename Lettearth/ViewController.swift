a8f33d0eba468c9b53310bb55f0e00d2a09aa68d
Ricky Effendi <rickricky.effendi@gmail.com>
2019-08-23 16:45:12 +0700

    Initial Commit

---
+++ Lettearth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lettearth
+//
+//  Created by Ricky Effendi on 23/08/19.
+//  Copyright © 2019 Ricky Effendi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
