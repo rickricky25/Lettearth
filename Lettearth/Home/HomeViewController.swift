72ef6c7892d5a3dd6a0bf7961434d36816aba3c2
Ricky Effendi <rickricky.effendi@gmail.com>
2019-08-28 13:18:39 +0700

    add feature to home

--- Lettearth/Home/HomeViewController.swift
+++ Lettearth/Home/HomeViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var CategoryCollection: UICollectionView!
     @IBOutlet weak var MaterialCollection: UICollectionView!
     
+    var imgCategory = ["imgCategoryKitchen", "imgCategoryKitchen"]
+    var lblCategory = ["Kitchen & Pantry", "Bathroom"]
+    
+    var imgMaterial = ["imgMaterialPlastic", "imgMaterialPlastic", "imgMaterialPlastic"]
+    var lblMaterial = ["Plastic", "Paper", "Wood"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,15 +29,32 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        var hasil = 0
         if collectionView == CategoryCollection {
-            
+            hasil = imgCategory.count
         } else if collectionView == MaterialCollection {
-            
+            hasil = imgMaterial.count
         }
+        return hasil
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        //var cell: UICollectionViewCell
+        if collectionView == CategoryCollection {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "identCategory", for: indexPath) as! CategoryCollectionViewCell
+            
+            cell.image.image = UIImage(named: imgCategory[indexPath.row])
+            cell.label.text = lblCategory[indexPath.row]
+            
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "identMaterial", for: indexPath) as! MaterialCollectionViewCell
+            
+            cell.image.image = UIImage(named: imgMaterial[indexPath.row])
+            cell.label.text = lblMaterial[indexPath.row]
+            
+            return cell
+        }
     }
     
     
